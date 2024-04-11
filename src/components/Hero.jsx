f370f3449f01f2443e8b4aedfaf2f4985ba71930
zablon <zablonoigo51@gmail.com>
2024-04-11 09:43:13 +0300

    Adding custom hook

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,33 +5,35 @@ function Hero() {
     const[active,setActive]=useState("Hostelry")
   return (
     <>
-    <div className=''>
-        <div className="">
-            <div className="">
-                <img src="" alt="" className="" />
+    <div className='container px-6 pt-12 mx-auto'>
+        <div className="raltive rounded-2xl lg:pb-32 lg:h-[70vh]">
+            <div className="relative w-full h-full overflow-hidden">
+                <img src="" alt="" className="lg:absolute top-0 w-full lg:h-[20vh] h-[30rem] object-cover rounded-2xl " />
             </div>
-            <div className="">
-                <h1 className="">
+            <div className="absolute top-0 bottom-0 left-0 right-0 h-fit">
+                <h1 className="flex justify-center mx-auto mt-16 text-4xl font-bold text-center text-white lg:w-4/5">
                     Explore the whole world and enjoy its beauty
                 </h1>
-                <p className="">
+                <p className="flex justify-center px-6 mt-5 text-center">
                     Find and write about your experiences around the world
                 </p>
             </div>
-            <div className="">
-                <div className="">
-                    <div className="">
+            <div className="lg:w-4/5 mx-auto lg:h-[70vh] h-full">
+                <div className="bg-white px-8 py-8 rounded-[10px] lg:absolute bottom-16 lg:w-4/5 shadow-lg">
+                    <div className="gap-4 lg:flex gap-x-2">
+                        <div className="flex flex-wrap w-full gap-x-16">
                         <div className="">
-                            <button className="" onClick={()=> setActive("Hostelry")}>Hostelry</button>
-                            <button className="" onClick={()=> setActive("Flights")}>Flights</button>
-                            <button className="" onClick={()=> setActive("Bust & Shuttle")}>Bus & Shuttle</button>
-                            <button className="" onClick={()=> setActive("Cars")}>Cars</button>
+                            <button className={`rounded-sm flex gap-x-2 items-center focus:outline-none h-14 text-base cursor-pointer bg-transparent whitespace-pre ${active === "Hostelry" ? "text-black border-b-2 border-solid border-black":"text-light"}`} onClick={()=> setActive("Hostelry")}>Hostelry</button>
+                            <button className={`rounded-sm flex gap-x-2 items-center focus:outline-none h-14 text-base cursor-pointer bg-transparent whitespace-pre ${active === "Flights" ? "text-black border-b-2 border-solid border-black":"text-light"}`}  onClick={()=> setActive("Flights")}>Flights</button>
+                            <button className={`rounded-sm flex gap-x-2 items-center focus:outline-none h-14 text-base cursor-pointer bg-transparent whitespace-pre ${active === "Bus & Shuttle" ? "text-black border-b-2 border-solid border-black":"text-light"}`}  onClick={()=> setActive("Bus & Shuttle")}>Bus & Shuttle</button>
+                            <button className={`rounded-sm flex gap-x-2 items-center focus:outline-none h-14 text-base cursor-pointer bg-transparent whitespace-pre ${active === "Cars" ? "text-black border-b-2 border-solid border-black":"text-light"}`}  onClick={()=> setActive("Cars")}>Cars</button>
                             
                         </div>
                         <button className="">
                             Last Searching
                             <FaArrowRight/>
                         </button>
+                        </div>
                     </div>
                     <div className="">
                         <p className="">Destination</p>
