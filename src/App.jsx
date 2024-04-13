224fa7463035a3e4f41f6492548be2d2c048dcf6
zablon <zablonoigo51@gmail.com>
2024-04-13 11:41:53 +0300

    Adding card component on root component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
-
+import Card  from "./components/Card"
 
 function App() {
 
@@ -9,6 +9,7 @@ function App() {
     <>
 <Navbar/>
 <Hero/>
+<Card/>
     </>
   )
 }
