8fe7f4a67593e00a773831d820841363de48b466
zablon <zablonoigo51@gmail.com>
2024-04-14 13:03:21 +0300

    Adding Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 import Card  from "./components/Card"
+import Footer from "./components/Footer"
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
 <Navbar/>
 <Hero/>
 <Card/>
+<Footer/>
     </>
   )
 }
