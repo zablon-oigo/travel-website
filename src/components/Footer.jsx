99e503311a0bd10a8acf5e5aa136f3ef19499799
zablon <zablonoigo51@gmail.com>
2024-04-14 13:14:37 +0300

    Footer boilerplate

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer className="px-10 py-10 ">
+        <div className="flex justify-between w-full">
+            <div className="">
+                <h2 className="text-2xl">Explore</h2>
+            </div>
+            <div className="flex gap-x-10">
+                <a href="" className="text-lg capitalize">home</a>
+                <a href="" className="text-lg capitalize">about</a>
+                <a href="" className="text-lg capitalize">contact</a>
+                <a href="" className="text-lg capitalize">services</a>
+            </div>
+        </div>
+    </footer>
   )
 }
 
