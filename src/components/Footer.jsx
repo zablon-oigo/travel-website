075d3994db273ed6f5ac95d820667195603183fb
zablon <zablonoigo51@gmail.com>
2024-04-14 13:20:22 +0300

    changed flex box direction on small device

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <footer className="px-10 py-10 ">
-        <div className="flex justify-between w-full">
+        <div className="flex flex-col items-center justify-between w-full md:flex-row">
             <div className="">
                 <h2 className="text-2xl font-bold">Explore</h2>
             </div>
