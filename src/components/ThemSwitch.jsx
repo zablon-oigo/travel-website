1d99bf3cfc4306d51ac6da1d3cb013c353c2e389
zablon <zablonoigo51@gmail.com>
2024-04-18 09:50:38 +0300

    Complete theme switch

--- src/components/ThemSwitch.jsx
+++ src/components/ThemSwitch.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { FaRegMoon } from "react-icons/fa";
+import { GoSun } from "react-icons/go";
 
-function ThemSwitch() {
+function ThemeSwitch() {
+  const [theme, setTheme] = useState(false);
+
+  const themeToggle = () => {
+    setTheme((prevMode) => !prevMode);
+  };
+
+  useEffect(() => {
+    const isDarkMode = localStorage.getItem('darkmode') === 'true';
+    setTheme(isDarkMode);
+  }, []);
+
+  useEffect(() => {
+    document.documentElement.classList.toggle('dark', theme);
+    localStorage.setItem('darkmode', theme);
+  }, [theme]);
+
   return (
-    <div>ThemSwitch</div>
-  )
+    <div className="z-10 px-4 text-2xl rounded-md cursor-pointer" onClick={themeToggle}>
+      {theme ? <GoSun className="text-white" /> : <FaRegMoon className="text-black" />}
+    </div>
+  );
 }
 
-export default ThemSwitch
+export default ThemeSwitch;
