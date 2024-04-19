f5dac4d72d51e80c1e9429b1d548bd3c77218d6e
zablon <zablonoigo51@gmail.com>
2024-04-19 13:04:02 +0300

    Adding date input

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,32 +23,36 @@ function Hero() {
             
             <div className="lg:w-4/5 mx-auto lg:h-[70vh] h-full">
             <div className="bg-white px-4 py-2 rounded-[10px] lg:absolute bottom-0 lg:w-4/5 shadow-lg ">
-                    <div className="">
-                        <div className="flex flex-wrap justify-center w-full gap-x-16">
-                        <div className="flex justify-between gap-x-16">
-                            <button className={`rounded-sm flex gap-x-2 items-center focus:outline-none h-14 text-base cursor-pointer bg-transparent whitespace-pre ${active === "Hostelry" ? "text-black border-b-2 border-solid border-black":"text-light"}`} onClick={()=> setActive("Hostelry")}>Hotel</button>
-                            <button className={`rounded-sm flex gap-x-2 items-center focus:outline-none h-14 text-base cursor-pointer bg-transparent whitespace-pre ${active === "Flights" ? "text-black border-b-2 border-solid border-black":"text-light"}`}  onClick={()=> setActive("Flights")}>Flights</button>
-                            <button className={`rounded-sm flex gap-x-2 items-center focus:outline-none h-14 text-base cursor-pointer bg-transparent whitespace-pre ${active === "Bus & Shuttle" ? "text-black border-b-2 border-solid border-black":"text-light"}`}  onClick={()=> setActive("Bus & Shuttle")}>Bus & Shuttle</button>
-                            <button className={`rounded-sm flex gap-x-2 items-center focus:outline-none h-14 text-base cursor-pointer bg-transparent whitespace-pre ${active === "Cars" ? "text-black border-b-2 border-solid border-black":"text-light"}`}  onClick={()=> setActive("Cars")}>Cars</button>
+                    
+                        <div className="flex flex-wrap w-full lg:justify-center gap-x-16">
+                        
+                           
+                           <button className={`rounded-sm   focus:outline-none h-10 lg:text-base text-xs cursor-pointer bg-transparent whitespace-pre ${active === "Hostelry" ? "text-black border-b-2 border-solid border-black":"text-light"}`} onClick={()=> setActive("Hostelry")}>Hotel</button>
+                            <button className={`rounded-sm   focus:outline-none h-10 lg:text-base text-xs cursor-pointer bg-transparent whitespace-pre ${active === "Flights" ? "text-black border-b-2 border-solid border-black":"text-light"}`}  onClick={()=> setActive("Flights")}>Flights</button>
+                            <button className={`rounded-sm   focus:outline-none h-10 lg:text-base text-xs cursor-pointer bg-transparent whitespace-pre ${active === "Bus & Shuttle" ? "text-black border-b-2 border-solid border-black":"text-light"}`}  onClick={()=> setActive("Bus & Shuttle")}>Bus & Shuttle</button>
+                            <button className={`rounded-sm   focus:outline-none h-10 lg:text-base text-xs cursor-pointer bg-transparent whitespace-pre ${active === "Cars" ? "text-black border-b-2 border-solid border-black":"text-light"}`}  onClick={()=> setActive("Cars")}>Cars</button>
+                           
+                          
                             
-                        </div>
+                        
                         <button className="flex items-center gap-2 whitespace-pre bg-transparent border-none outline-none text-light">
                             Last Searching
                             <FaArrowRight/>
                         </button>
                         </div>
-                    </div>
-                    <div className="flex items-center gap-x-16">
-                    <div className="flex flex-wrap items-start justify-between gap-4 py-4 border-t border-solid">
+                    
+                    <div className="">
+                    <div className="border-t border-solid ">
                         <div className="">
                         <p className="text-lg text-light">Destination</p>
+                        
+                        </div>
+                        <section className="grid grid-cols-5 my-8 place-content-center ">
                         <div className="relative flex items-center w-40 h-10">
                             <FaLocationDot className='absolute top-0 bottom-0 z-10 h-full left-2 text-primary'/>
                             <input type='text' placeholder="Bali, Indonesia" className='absolute h-full px-2 py-1 pl-6 text-sm rounded-md outline-none bg-light1'/>
 
                         </div>
-                        </div>
-                        <section className="grid grid-cols-4 my-8 ">
                         <div className="relative flex w-40 h-10">
                             <FaCalendarAlt className='absolute top-0 bottom-0 z-10 h-full left-2 text-primary'/>
                             <input type='date' defaultValue={"2023-04-11"} className='absolute h-full px-2 py-1 pl-6 text-sm rounded-md outline-none bg-light1'/>
